24829971447a5f33390e58d20b81369335d59802
mjdelgadog <99819198+mjdelgadog@users.noreply.github.com>
2023-03-28 11:12:02 -0500

    Update OpHitAlg_deco.cxx

--- Ophitfinder/OpHitAlg_deco.cxx
+++ Ophitfinder/OpHitAlg_deco.cxx
@@ -26,7 +26,8 @@
 #include <vector>
 
 namespace opdet {
-  //----------------------------------------------------------------------------
+
+ 
   void RunHitFinder(std::vector<raw::OpDetWaveform> const& opDetWaveformVector,
                     std::vector<recob::OpHit>& hitVector,
                     pmtana::PulseRecoManager const& pulseRecoMgr,
@@ -56,7 +57,8 @@ namespace opdet {
       const double timeStamp = waveform.TimeStamp();
 
       for (auto const& pulse : pulses)
-        ConstructHit(hitThreshold,
+        ConstructHit(opDetWaveformVector,
+                     hitThreshold,
                      channel,
                      timeStamp,
                      pulse,
@@ -69,6 +71,7 @@ namespace opdet {
 
   //----------------------------------------------------------------------------
   void RunHitFinder_deco(std::vector<recob::OpWaveform>const& opWaveformVector,
+                    std::vector<raw::OpDetWaveform> const& opDetWaveformVector,
                     std::vector<recob::OpHit>& hitVector,
                     pmtana::PulseRecoManager const& pulseRecoMgr,
                     pmtana::PMTPulseRecoBase const& threshAlg,
@@ -78,32 +81,33 @@ namespace opdet {
                     calib::IPhotonCalibrator const& calibrator,
                     bool use_start_time)
   {
-
-    for (auto const& deco_waveform : opWaveformVector) {
-
-      const int channel = static_cast<int>(deco_waveform.Channel());
-
-      if (!geometry.IsValidOpChannel(channel)) {
-        mf::LogError("OpHitFinder")
+     
+     for (int i=0; i< int (opWaveformVector.size()); i++){
+        recob::OpWaveform deco_waveform=opWaveformVector.at(i);
+        raw::OpDetWaveform waveform=opDetWaveformVector.at(i);
+        int channel = static_cast<int>(deco_waveform.Channel()); 
+        const double timeStamp = waveform.TimeStamp();
+        
+        if (!geometry.IsValidOpChannel(channel)) {
+          mf::LogError("OpHitFinder")
           << "Error! unrecognized channel number " << channel << ". Ignoring pulse";
-        continue;
-      }
+         continue;
+        }
       
-      std::vector<short int> short_deco_waveform;
-      for (unsigned int i_tick=0; i_tick < deco_waveform.Signal().size(); ++i_tick)
-      {
-      short_deco_waveform.emplace_back(static_cast<short int>(deco_waveform.Signal().at(i_tick)));
-      }
+       std::vector<short int> short_deco_waveform;
+       for (unsigned int i_tick=0; i_tick < deco_waveform.Signal().size(); ++i_tick)
+       {
+       short_deco_waveform.emplace_back(static_cast<short int>(deco_waveform.Signal().at(i_tick)));
+       }
          
-      pulseRecoMgr.Reconstruct(short_deco_waveform);
-      
+       pulseRecoMgr.Reconstruct(short_deco_waveform);
+      //pulseRecoMgr.Reconstruct(deco_waveform); 
+
       // Get the result
       auto const& pulses = threshAlg.GetPulses();
-
-      double timeStamp = double (deco_waveform.TimeStamp().GetTimeStamp());
-
       for (auto const& pulse : pulses)
-        ConstructHit(hitThreshold,
+        ConstructHit(opDetWaveformVector,
+                     hitThreshold,
                      channel,
                      timeStamp,
                      pulse,
@@ -111,10 +115,12 @@ namespace opdet {
                      clocksData,
                      calibrator,
                      use_start_time);
-    }
-  }
+     }
+ }
+  
   //----------------------------------------------------------------------------
-  void ConstructHit(float hitThreshold,
+  void ConstructHit(std::vector<raw::OpDetWaveform> const&,
+                    float hitThreshold,
                     int channel,
                     double timeStamp,
                     pmtana::pulse_param const& pulse,
@@ -126,25 +132,25 @@ namespace opdet {
 
     if (pulse.peak < hitThreshold) return;
 
-    double absTime = timeStamp + clocksData.OpticalClock().TickPeriod() *
+     double absTime = timeStamp + clocksData.OpticalClock().TickPeriod() *
                                    (use_start_time ? pulse.t_start : pulse.t_max);
 
-    double relTime = absTime - clocksData.TriggerTime();
+     double relTime = absTime - clocksData.TriggerTime();
 
-    double startTime =
+     double startTime =
       timeStamp + clocksData.OpticalClock().TickPeriod() * pulse.t_start - clocksData.TriggerTime();
 
-    double riseTime = clocksData.OpticalClock().TickPeriod() * pulse.t_rise;
+     double riseTime = clocksData.OpticalClock().TickPeriod() * pulse.t_rise;
 
-    int frame = clocksData.OpticalClock().Frame(timeStamp);
+     int frame = clocksData.OpticalClock().Frame(timeStamp);
 
-    double PE = 0.0;
-    if (calibrator.UseArea())
-      PE = calibrator.PE(pulse.area, channel);
-    else
-      PE = calibrator.PE(pulse.peak, channel);
+     double PE = 0.0;
+     //if (calibrator.UseArea())
+       PE = calibrator.PE(pulse.area, channel);
+     //else
+     // PE = calibrator.PE(pulse.peak, channel);
 
-    double width = (pulse.t_end - pulse.t_start) * clocksData.OpticalClock().TickPeriod();
+     double width = (pulse.t_end - pulse.t_start) * clocksData.OpticalClock().TickPeriod();
 
     hitVector.emplace_back(channel,
                            relTime,

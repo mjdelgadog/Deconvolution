82b9caed2817a519dcf6eaa2521cfa912dfbe4a5
mjdelgadog <99819198+mjdelgadog@users.noreply.github.com>
2023-04-11 06:47:00 -0500

    Laura's comments
    This part was made by you or was it already there? You could include the original file info and add your names if any modification was made.

--- Ophitfinder/OpHitAlg_deco.cxx
+++ Ophitfinder/OpHitAlg_deco.cxx
@@ -9,6 +9,12 @@
  *
  * Description:
  * These are the algorithms used by OpHitFinder to produce optical hits.
+ * 
+ * Editors: Daniele Guffanti,Maritza Delgado,Sergio Manthey.
+ *
+ * Description:
+ * recob::OpWaveform object has been included inside the 
+ * RunHitFinder_deco function. 
  */
 
 #include "OpHitAlg_deco.h"
@@ -77,50 +83,89 @@ namespace opdet {
                     pmtana::PMTPulseRecoBase const& threshAlg,
                     geo::GeometryCore const& geometry,
                     float hitThreshold,
-                    float scale,
+                    float scale,  //It scales the values of the deconvolved signals.
                     detinfo::DetectorClocksData const& clocksData,
                     calib::IPhotonCalibrator const& calibrator,
                     bool use_start_time)
   {
-     
-    for (int i=0; i< int (opWaveformVector.size()); i++){
-      recob::OpWaveform deco_waveform=opWaveformVector.at(i);
-      raw::OpDetWaveform waveform=opDetWaveformVector.at(i);
-      int channel = static_cast<int>(deco_waveform.Channel()); 
-      const double timeStamp = waveform.TimeStamp();
-        
-      if (!geometry.IsValidOpChannel(channel)) {
-        mf::LogError("OpHitFinder")
-        << "Error! unrecognized channel number " << channel << ". Ignoring pulse";
-        continue;
-      }
       
-      std::vector<short int> short_deco_waveform;
+     for (int i=0; i< int (opWaveformVector.size()); i++){
+        recob::OpWaveform deco_waveform=opWaveformVector.at(i);
+        int channel = static_cast<int>(deco_waveform.Channel());
+        //The raw timestamp is used
+        raw::OpDetWaveform waveform=opDetWaveformVector.at(i);
+        const double timeStamp = waveform.TimeStamp();
+       
+        if (!geometry.IsValidOpChannel(channel)) {
+          mf::LogError("OpHitFinder")
+          << "Error! unrecognized channel number " << channel << ". Ignoring pulse";
+         continue;
+        }
       
-      for (unsigned int i_tick=0; i_tick < deco_waveform.Signal().size(); ++i_tick){
-        short_deco_waveform.emplace_back(static_cast<short int>(scale*deco_waveform.Signal().at(i_tick)));}
-        
-      pulseRecoMgr.Reconstruct(short_deco_waveform);
-
+       // Loop to convert from float to short
+       std::vector<short int> short_deco_waveform; //vector used to convert from float to short.
+       for (unsigned int i_tick=0; i_tick < deco_waveform.Signal().size(); ++i_tick)
+       {
+       short_deco_waveform.emplace_back(static_cast<short int>(scale*deco_waveform.Signal().at(i_tick)));
+       }
+         
+       pulseRecoMgr.Reconstruct(short_deco_waveform);
+      
       // Get the result
       auto const& pulses = threshAlg.GetPulses();
-      for (auto const& pulse : pulses) ConstructHit(opDetWaveformVector,hitThreshold,channel,timeStamp,pulse,hitVector,clocksData,calibrator,use_start_time);
-  }
-}
+      for (auto const& pulse : pulses)
+        ConstructHit(opDetWaveformVector,
+                     hitThreshold,
+                     channel,
+                     timeStamp,
+                     pulse,
+                     hitVector,
+                     clocksData,
+                     calibrator,
+                     use_start_time);
+     }
+ }
   
   //----------------------------------------------------------------------------
-  void ConstructHit(std::vector<raw::OpDetWaveform> const&,float hitThreshold,int channel,double timeStamp,pmtana::pulse_param const& pulse,std::vector<recob::OpHit>& hitVector,detinfo::DetectorClocksData const& clocksData,calib::IPhotonCalibrator const& calibrator,bool use_start_time){
+  void ConstructHit(std::vector<raw::OpDetWaveform> const&,
+                    float hitThreshold,
+                    int channel,
+                    double timeStamp,
+                    pmtana::pulse_param const& pulse,
+                    std::vector<recob::OpHit>& hitVector,
+                    detinfo::DetectorClocksData const& clocksData,
+                    calib::IPhotonCalibrator const& calibrator,
+                    bool use_start_time)
+  {
+
     if (pulse.peak < hitThreshold) return;
-    double absTime = timeStamp + clocksData.OpticalClock().TickPeriod()*(use_start_time ? pulse.t_start : pulse.t_max);
-    double relTime = absTime - clocksData.TriggerTime();
-    double startTime = timeStamp + clocksData.OpticalClock().TickPeriod() * pulse.t_start - clocksData.TriggerTime();
-    double riseTime = clocksData.OpticalClock().TickPeriod() * pulse.t_rise;
-    int frame = clocksData.OpticalClock().Frame(timeStamp);
-    double PE = 0.0;
 
-    PE = calibrator.PE(pulse.area, channel);
-    double width = (pulse.t_end - pulse.t_start) * clocksData.OpticalClock().TickPeriod();
+     double absTime = timeStamp + clocksData.OpticalClock().TickPeriod() * (use_start_time ? pulse.t_start : pulse.t_max);
+     double relTime = absTime - clocksData.TriggerTime();
+     double startTime = timeStamp + clocksData.OpticalClock().TickPeriod() * pulse.t_start - clocksData.TriggerTime();
+     double riseTime = clocksData.OpticalClock().TickPeriod() * pulse.t_rise;
+     int frame = clocksData.OpticalClock().Frame(timeStamp);
+     double PE = 0.0;
 
-    hitVector.emplace_back(channel,relTime,absTime,startTime,riseTime,frame,width,pulse.area,pulse.peak,PE,0.0);
+     if (calibrator.UseArea())   
+       PE = calibrator.PE(pulse.area, channel);
+     else    
+      PE = calibrator.PE(pulse.peak, channel);   
+
+     double width = (pulse.t_end - pulse.t_start) * clocksData.OpticalClock().TickPeriod();
+
+     hitVector.emplace_back(channel,
+                           relTime,
+                           absTime,
+                           startTime,
+                           riseTime,
+                           frame,
+                           width,
+                           pulse.area,
+                           pulse.peak,
+                           PE,
+                           0.0);
   }
+  
+
 } // End namespace opdet

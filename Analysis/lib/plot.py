5838227e01cd0546c98fb870d93926380aaf2817
Sergio Manthey Corchado <70066932+mantheys@users.noreply.github.com>
2023-05-09 22:38:36 +0200

    Update plot.py

--- Analysis/lib/plot.py
+++ Analysis/lib/plot.py
@@ -24,17 +24,27 @@ def gauss_fit_distribution(df,variable,color,color_map,xlim=(-100,100),acc=100):
         plot_dict = {"filter": filter, "hist":hist, "bin_centers":bin_centers, "fit": fit}
         plot_array.append(plot_dict)
 
-    plot_df = pd.DataFrame(plot_array).explode(["hist","bin_centers","fit"])
-    fig = px.bar(plot_df,
-                x="bin_centers",
-                y="hist", 
-                color="filter", 
-                color_discrete_sequence=pd.Series(plot_df["filter"].unique()).map(color_map),
-                barmode="overlay", 
-                template="presentation",
-                )
-    for idx,filter in enumerate(df[color].unique()):
-        fig.add_trace(go.Scatter(name="FIT "+filter,x=plot_df[plot_df["filter"] == filter]["bin_centers"], y=plot_df[plot_df["filter"] == filter]["fit"],line=dict(color=color_map[filter])))
-        # fig.add_vline(x=np.mean(df[df[color] == filter][variable]))
+    try:
+        plot_df = pd.DataFrame(plot_array).explode(["hist","bin_centers","fit"])
+        fig = px.bar(plot_df,
+                    x="bin_centers",
+                    y="hist", 
+                    color="filter", 
+                    color_discrete_sequence=pd.Series(plot_df["filter"].unique()).map(color_map),
+                    barmode="overlay", 
+                    template="presentation",
+                    )
+        for idx,filter in enumerate(df[color].unique()):
+            fig.add_trace(go.Scatter(name="FIT "+filter,x=plot_df[plot_df["filter"] == filter]["bin_centers"], y=plot_df[plot_df["filter"] == filter]["fit"],line=dict(color=color_map[filter])))
+            # fig.add_vline(x=np.mean(df[df[color] == filter][variable]))
+    except:
+        fig = plt.figure(figsize=(10,5))
+        for plot in plot_array:
+            plt.bar(plot["bin_centers"], plot["hist"], color=color_map[plot["filter"]])
+            plt.plot(plot["bin_centers"], plot["fit"], label=plot["filter"], color=color_map[plot["filter"]])
+        plt.ylabel("Counts"); plt.xlabel(variable)
+        plt.grid()
+        plt.legend()
+        # plt.show()
 
     return fig
